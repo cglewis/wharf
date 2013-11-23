163afe29dacac1f30fce29eb16400a050652fcd0
clewis <clewis@iqt.org>
2013-11-23 08:04:03 +0000

    add pieces for users, not enforced in templates yet

--- api.py
+++ api.py
@@ -1,10 +1,18 @@
 from flask import Flask
 from flask import Markup
+from flask import Response
+from flask import jsonify
 from flask import render_template
 from flask import request
-from flask import jsonify
+from flask import session
+from flask.ext.babel import Babel
+from flask.ext.mail import Mail
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.security import Security
+from flask.ext.security import SQLAlchemyUserDatastore
+from flask.ext.security import UserMixin
+from flask.ext.security import RoleMixin
 from functools import wraps
-from flask import request, Response
 
 from docker import client
 
@@ -16,7 +24,25 @@ import redis
 import time
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
+# this should re-generated for production use
+app.config['SECRET_KEY'] = 'EckNi2Fluincawd+'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/db.sqlite'
+app.config['DEFAULT_MAIL_SENDER'] = 'dockerwharf@gmail.com'
+app.config['SECURITY_REGISTERABLE'] = True
+app.config['SECURITY_CONFIRMABLE'] = True
+app.config['SECURITY_RECOVERABLE'] = True
+app.config['SECURITY_PASSWORD_HASH'] = 'sha512_crypt'
+# this should re-generated for production use
+app.config['SECURITY_PASSWORD_SALT'] = 'S)1<P3_~$XF}DI=#'
+app.config.from_object('config.email')
 app.debug = True
+
+# Setup mail extension
+mail = Mail(app)
+
+# Setup babel
+babel = Babel(app)
 
 #set defaults
 
@@ -41,6 +67,47 @@ DOCKER_HOST="localhost"
 
 r = redis.StrictRedis(host=REDIS_HOST, port=int(REDIS_PORT))
 c = client.Client(version="1.6", base_url='http://%s:4243' % DOCKER_HOST)
+
+@babel.localeselector
+def get_locale():
+    override = request.args.get('lang')
+
+    if override:
+        session['lang'] = override
+
+    rv = session.get('lang', 'en')
+    return rv
+
+# Create database connection object
+db = SQLAlchemy(app)
+
+# Define models
+roles_users = db.Table('roles_users',
+        db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+        db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
+
+    def __str__(self):
+        return '<User id=%s email=%s>' % (self.id, self.email)
+
+# Setup Flask-Security
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
+
+db.create_all()
 
 def check_auth(username, password):
     """This function is called to check if a username /
---
+++ config/email.py.sample
@@ -0,0 +1,6 @@
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+MAIL_USERNAME = 'gmail_email_address'
+MAIL_PASSWORD = 'gmail_password'
